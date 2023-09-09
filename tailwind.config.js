939c1846eb2dc43117bdcdb1ceba1ced165b7ff2
Habib Afolabi <habibwritescode@gmail.com>
2023-09-09 23:25:29 +0100

    config: add colors to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,20 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      transparent: "transparent",
+      white: "#ffffff",
+      black: "#000000",
+      blue: "#3F5BF6",
+      gray: {
+        900: "#101828",
+        700: "#344054",
+        600: "#475467",
+        500: "#667085",
+        300: "#D0D5DD",
+        normal: "#272727",
+      },
+    },
   },
   plugins: [],
 };
