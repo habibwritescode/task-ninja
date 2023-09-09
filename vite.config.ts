861f13a73bab97e5de487d5e7eac6e71b6035457
Habib Afolabi <habibwritescode@gmail.com>
2023-09-09 22:55:15 +0100

    setup: initial setup with vite + tailwind

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+/// <reference types="vite/client" />
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [svgr(), react()],
+
+  server: {
+    port: 3001,
+    open: true,
+  },
+});
